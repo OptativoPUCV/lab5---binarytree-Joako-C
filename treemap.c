dfef8025ba4fd6f1ba2d78a7cacfebcd917689b7
Replit user <>
2023-09-14 03:44:14 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -56,14 +56,11 @@ void insertTreeMap(TreeMap* tree, void* key, void* value) {
 
     if (new_node == NULL) return;
 
-    if (tree->root == NULL) {
-        tree->root = tree->current = new_node;
-        return;
-    }
-
     TreeNode* current = tree->root;
+    TreeNode* parent = NULL;
 
     while (current != NULL) {
+        parent = current;
         int comparison = tree->lower_than(key, current->pair->key);
 
         if (comparison == 0) {
@@ -74,25 +71,26 @@ void insertTreeMap(TreeMap* tree, void* key, void* value) {
             free(new_node);
             return;
         } else if (comparison < 0) {
-            if (current->left == NULL) {
-                current->left = new_node;
-                new_node->parent = current;
-                tree->current = new_node;
-                return;
-            }
             current = current->left;
         } else {
-            if (current->right == NULL) {
-                current->right = new_node;
-                new_node->parent = current;
-                tree->current = new_node;
-                return;
-            }
             current = current->right;
         }
     }
-}
 
+    if (parent == NULL) {
+        tree->root = new_node;
+    } else {
+        int comparison = tree->lower_than(key, parent->pair->key);
+        if (comparison < 0) {
+            parent->left = new_node;
+        } else {
+            parent->right = new_node;
+        }
+        new_node->parent = parent;
+    }
+
+    tree->current = new_node;
+}
 
 
 
