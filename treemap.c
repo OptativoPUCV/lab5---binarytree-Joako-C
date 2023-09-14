f38e4afae903a6803ce6f571044af41eec333288
Replit user <>
2023-09-14 03:43:26 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -95,6 +95,18 @@ void insertTreeMap(TreeMap* tree, void* key, void* value) {
 
 
 
+
+TreeNode* minimum(TreeNode* x) {
+    if (x == NULL) return NULL;
+
+    while (x->left != NULL) {
+        x = x->left;
+    }
+  
+    return x;
+}
+
+
 void removeNode(TreeMap * tree, TreeNode* node) {
     if (tree == NULL || node == NULL || tree->root == NULL) return;
 
